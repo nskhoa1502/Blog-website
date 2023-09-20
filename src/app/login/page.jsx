aade49c6395a7fc6a1db18a7356ef6aeb6dc7e6b
haha <nskhoa1502@gmail.com>
2023-09-20 21:02:25 +0700

    add-prisma-mongodb

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,10 +3,20 @@
 import React from "react";
 import styles from "./LoginPage.module.css";
 import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
   const { data, status } = useSession();
+  const router = useRouter();
   console.log(data, status);
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+    router.push("/");
+  }
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
